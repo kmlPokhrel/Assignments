8235b2cb09e6a965658269fa132a639d66e1007a
Kamal Pokhrel <129597450+kmlPokhrel@users.noreply.github.com>
2024-02-03 01:07:25 +0530

    Add files via upload

---
+++ 1st prints.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main(){
+    printf("\"Hello world\""); //   \"Your text\"
+   // printf("\n\"");  //print \name
+    printf("\n");
+
+
+   printf("\\n");
+
+
+   printf("\n");
+
+   printf("\"MysirG\"");
+
+
+   printf("\n");
+
+   printf("\\r");
+
+
+ printf("\n");
+
+   printf("\"Teacher's Day\"");
+   
+}
